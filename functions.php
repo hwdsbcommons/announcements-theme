4cd2ab1bc94f7c8d527d0b888b131bc437d6e6f9
Jared Bennett <jarbenne@hwdsb.on.ca>
2016-09-04 14:20:58 -0400

    Unpublish
    Along with this plugin from Humanmade active on the site, allows users to set an expiry date on slides: https://github.com/humanmade/Unpublish

--- functions.php
+++ functions.php
@@ -4,3 +4,9 @@ add_theme_support( 'post-thumbnails' );
 
 /* Disable WordPress Admin Bar */
 add_filter('show_admin_bar', '__return_false');
+
+/* Add support to allow slides to be unpublished https://github.com/humanmade/Unpublish */
+$types = array( 'post', 'page', 'slides' );
+foreach( $types as $type ) {
+    add_post_type_support( $type, 'unpublish' );
+}
